1b321071faae53a89c1eda762c4d86a6ec9f3e2f
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-31 11:38:01 +0300

    Added product model

---
+++ EStore.MVC/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace EStore.MVC.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }       
+        public string Name { get; set; }        
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
